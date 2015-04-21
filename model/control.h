a085040b1fdc7733509c7cd76695e33ed10ef584
itmo153277 <153277@niuitmo.ru>
2015-04-21 10:18:00 +0300

    +ControlSystem base class

---
+++ model/control.h
@@ -0,0 +1,49 @@
+/*
+ vprobot
+ Copyright (C) 2015 Ivanov Viktor
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __CONTROL_H_
+#define __CONTROL_H_
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "robot.h"
+
+namespace vprobot {
+
+namespace control {
+
+/* Базовый класс системы управления */
+class CControlSystem {
+private:
+	CControlSystem(const CControlSystem &ControlSystem) = default;
+public:
+	CControlSystem() = default;
+	virtual ~CControlSystem() = default;
+
+	/* Получить команду */
+	virtual const vprobot::robot::Control * const GetCommands(
+			const vprobot::robot::SMeasures * const Mesuarements) = 0;
+};
+
+}
+
+}
+
+#endif
