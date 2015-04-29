8ecfcaf8cf9b37d27e6a9a5bebf3b592bef21148
itmo153277 <153277@niuitmo.ru>
2015-04-29 18:15:01 +0300

    +EKF prediction step

--- model/localization/ekf.cpp
+++ model/localization/ekf.cpp
@@ -21,7 +21,11 @@
 #include <cstddef>
 #include <cmath>
 
+#include "../../types.h"
+
 using namespace ::std;
+using namespace ::Eigen;
+using namespace ::vprobot;
 using namespace ::vprobot::robot;
 using namespace ::vprobot::presentation;
 using namespace ::vprobot::control;
@@ -32,7 +36,7 @@ using namespace ::vprobot::control::localization;
 vprobot::control::localization::CEKFLocalization::CEKFLocalization(
 		const Json::Value &ControlSystemObject) :
 		CSequentialControlSystem(ControlSystemObject), m_States() {
-	m_Radius = ControlSystemObject["radius"].asDouble();
+	m_Radius = 1 / ControlSystemObject["radius"].asDouble();
 	m_DRadius = ControlSystemObject["dradius"].asDouble();
 	m_Len = ControlSystemObject["len"].asDouble();
 	m_DLen = ControlSystemObject["dlen"].asDouble();
@@ -67,7 +71,61 @@ void vprobot::control::localization::CEKFLocalization::DrawPresentation(
 /* Обработка */
 void vprobot::control::localization::CEKFLocalization::Process(
 		const SMeasures * const *Measurements) {
+	if (m_LastCommand != NULL) {
+		size_t i;
+		for (i = 0; i < m_Count; i++) {
+			if (m_LastCommand[i] == Nothing)
+				continue;
 
+			Vector2d u;
+			Matrix2d CovControl;
+			Matrix3d G;
+			Matrix<double, 3, 2> V;
+
+			switch (m_LastCommand[i]) {
+				case Forward:
+					u << m_Len, 0;
+					break;
+				case ForwardLeft:
+					u << m_Len, m_Radius;
+					break;
+				case ForwardRight:
+					u << m_Len, -m_Radius;
+					break;
+				case Backward:
+					u << -m_Len, 0;
+					break;
+				case BackwardLeft:
+					u << -m_Len, m_Radius;
+					break;
+				case BackwardRight:
+					u << -m_Len, -m_Radius;
+					break;
+				default:
+					break;
+			}
+			double nangle = m_States[i].s_MeanState[2];
+			double dx, dy;
+
+			if (EqualsZero(u[1])) {
+				dx = u[0] * cos(nangle);
+				dy = u[0] * sin(nangle);
+				V << cos(nangle), -u[0] * dy / 2, sin(nangle), u[0] * dx / 2, 0, u[0];
+			} else {
+				nangle = CorrectAngle(nangle + u[0] * u[1]);
+				dx = (sin(nangle) - sin(m_States[i].s_MeanState[2])) / u[1];
+				dy = (cos(m_States[i].s_MeanState[2]) - cos(nangle)) / u[1];
+				V << cos(nangle), (u[0] * cos(nangle) - dx) / u[1], sin(nangle), (u[0]
+						* sin(nangle) - dy) / u[1], u[1], u[0];
+			}
+			G << 1, 0, -dy, 0, 1, dx, 0, 0, 1;
+			CovControl << m_DLen * m_DLen, 0, 0, m_DRadius * m_DRadius;
+			m_States[i].s_CovState = G * m_States[i].s_CovState * G.transpose()
+					+ V * CovControl * V.transpose();
+			m_States[i].s_MeanState << m_States[i].s_MeanState[0] + dx, m_States[i].s_MeanState[1]
+					+ dy, nangle;
+		}
+	}
 }
 
 /* Получить команду */
